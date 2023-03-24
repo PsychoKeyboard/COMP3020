eb94a755c9447729ce6d2241c87daa0f194d57f1
krupali2000 <krupsp2000@gmail.com>
2023-03-23 19:22:02 -0500

    tests

---
+++ krupali/searchFunction.js
@@ -0,0 +1,7 @@
+function searchFunction(){
+    //filter through recipes and guve priority num according to num of ingredients
+    //if ingredient is missing, priority num is decreased by 1
+    //super ingredients have a num of 2
+    //normal ingredients have a num of 1
+    //~math~ 
+}
