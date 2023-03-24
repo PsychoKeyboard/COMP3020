026db49475b95cd6ce77bd6478bcb37ef833ac72
krupali2000 <krupsp2000@gmail.com>
2023-03-24 17:10:57 -0500

    Create database.js

---
+++ krupali/database.js
@@ -0,0 +1,21 @@
+const allIngredientsList = new Array();
+
+//Everyday Items
+//Dairy
+milk = new Ingredient("Milk",1);
+eggs = new Ingredient("Eggs",1);
+butter = new Ingredient("Butter/Margerine",1);
+yogurt = new Ingredient("Yogurt",1);
+butterMilk = new Ingredient("Butter Milk",1);
+whippedCream = new Ingredient("Whipped Cream",1);
+goatCheese = new Ingredient("Goat Cheese",1);
+iceCream = new Ingredient("Ice Cream",1);
+sourCream = new Ingredient("Sour Cream",1);
+condensedMilk = new Ingredient("Condensed Milk",1);           
+halfAndHalf = new Ingredient("Half and Half",1);
+coconutMilk = new Ingredient("Coconut Milk",1);
+heavyCream = new Ingredient("Heavy Cream",1);
+
+//Pasta and Grains
+
+
