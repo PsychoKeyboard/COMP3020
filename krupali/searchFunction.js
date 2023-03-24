d1254c48e87ba0a7a4ae98a87282f9f4133bd95b
krupali2000 <krupsp2000@gmail.com>
2023-03-24 12:18:22 -0500

    Better search pseudo code

--- krupali/searchFunction.js
+++ krupali/searchFunction.js
@@ -1,7 +1,15 @@
-function searchFunction(){
+function searchFunction(ingredientsList, recipeList){
     //filter through recipes and guve priority num according to num of ingredients
     //if ingredient is missing, priority num is decreased by 1
     //super ingredients have a num of 2
     //normal ingredients have a num of 1
     //~math~ 
+    for(var i = 0; i < recipeList.length; i++){
+        recipe = recipeList[i];
+        for(var j =0; j < recipe.ingredients.length; j++){
+            //if recipe.ingredient[j] is in List, recipe.priority += ingredient.priority;
+        }
+    }
+    return recipeList;
 }
+
