c0a683071e3f15fa5cb5a9cd209b54a7a1ae72e0
krupali2000 <krupsp2000@gmail.com>
2023-03-25 15:54:55 -0500

    pushing check function

--- krupali/test.js
+++ krupali/test.js
@@ -1,1 +1,27 @@
-document.getElementById("milk").innerHTML = "this";
+document.getElementById("cheese").innerHTML = "this";
+
+class Ingredient{
+    constructor(name, priority){
+        this.name = name;
+        this.checked = flase;
+        this.priority = priority;
+    }
+    
+    check(){
+        if (this.checked = false){
+            this.checked = true;
+            this.priority = 1;
+        }
+        if (this.checked = true){
+            this.checked = false;
+            this.priority = 0;
+        }
+    }
+}
+
+var cheese = new Ingredient("cheese", 0);
+
+function clicked(){
+    document.getElementById("cheese").innerHTML = "Cheese is checked? " + cheese.checked;
+    cheese.check();
+}
