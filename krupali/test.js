63d74f70962cf5d50a8c8ef024c4aa1fdc5377f6
krupali2000 <krupsp2000@gmail.com>
2023-03-25 17:25:53 -0500

    variable declaration change

--- krupali/test.js
+++ krupali/test.js
@@ -1,25 +1,8 @@
 document.getElementById("cheese").innerHTML = "this";
 
+var cheese;
+
 function clicked(){
-    class Ingredient{
-        constructor(name, priority){
-            this.name = name;
-            this.checked = false;
-            this.priority = priority;
-        }
-        
-        check(){
-            if (this.checked = false){
-                this.checked = true;
-                this.priority = 1;
-            }
-            else if (this.checked = true){
-                this.checked = false;
-                this.priority = 0;
-            }
-        }
-    }
-    var cheese = new Ingredient("cheese", 0);
     cheese.check();
     document.getElementById("cheese").innerHTML = "Cheese is checked?" + cheese.checked;
 }
@@ -33,11 +16,11 @@ function generateAll(){
         }
         
         check(){
-            if (this.checked = false){
+            if (this.checked == false){
                 this.checked = true;
                 this.priority = 1;
             }
-            else if (this.checked = true){
+            else if (this.checked == true){
                 this.checked = false;
                 this.priority = 0;
             }
@@ -45,10 +28,12 @@ function generateAll(){
     }
 
     class Recipe{
-        constructor(recipeName, ingredientsList, priority){
+        constructor(recipeName, ingredientsList, priority, url, image){
             this.name = recipeName;
             this.ingredients = ingredientsList;
             this.priority = priority;
+            this.url = url;
+            this.image = image;
         }
     
         add(ingredient){
@@ -62,19 +47,15 @@ function generateAll(){
         }
     }
 
-    generateIngredients();
+    cheese = new Ingredient("cheese", 0);
     generateRecipes();
-
+    
 }
 
 function generateIngredients(){
-    var cheese = new Ingredient("cheese", 0);
-    var peanutButter = new Ingredient("Peanut Butter", 0);
-    var jam = new Ingredient("Jam", 0);
-    var brad = new Ingredient("Bread", 0);
+    
 }
 
 function generateRecipes(){
-    var ingredientList = [peanutButter, Jam, Bread]
-    var pbjSandwich = new Recipe("Peanut Butter and Jam Sandwich", ingredientList, 0 )
+    recipeName = new Recipe("Peanut Butter and Jam Sandwich", [peanutButter, Jam, Bread], 0,);
 }
